61152c7cd6eee56f95d9582abc4bbdbd980a91a8
Juan Leon <ricleongo@gmail.com>
2019-02-08 10:09:03 -0500

    Latest changes on files structure and analysis

---
+++ assistant_config/config.py
@@ -0,0 +1,9 @@
+
+watson_username='apikey'
+watson_password='Fb8L_OVwYjtH0EtTLzpefV4CVPun_yUfAU_3Typ-Skcc'
+watson_url='https://gateway.watsonplatform.net/assistant/api'
+watson_version='2018-09-20'
+watson_assistanceID='366bbb40-4084-48a3-9e6b-7fa935dcee13'
+
+account_sid = "AC239e0eead3f1872ec3768a2064da5b3f"
+auth_token  = "679cc165d51a883a20af6ffac88c0ee6"
